eea1bfea97503586eec5f8b8aa3dc86834982b05
Diana Oxina <oxinadiana@gmail.com>
2026-09-06 19:23:21 +0800

    Final Update

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,13 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5175',
+  origin: ['http://localhost:5173', 'http://localhost:5175'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
 
 app.use(express.json());
 
